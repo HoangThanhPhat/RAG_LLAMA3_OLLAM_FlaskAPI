992d8e2f7b0b514a36f6a040ad2d072edb69065c
thanhphathoang <125521127+HoangThanhPhat@users.noreply.github.com>
2024-05-03 17:45:46 +0700

    crt_db.py

--- crt_db.py
+++ crt_db.py
@@ -1,13 +1,22 @@
-from app import database
+# from app import database
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+from flask_sqlalchemy import SQLAlchemy
 
+database = SQLAlchemy()
 class User(database.Model):
-    __name__ = "user"
-    id       = Column(Integer, primary_key=True, autoincrement=True)
-    name     = Column(String(50), nullable= False)
-    qnas     = relationship("Qna", backref="user", lazy= False)
-
+    __name__    = "user"
+    id          = Column(Integer, primary_key=True, autoincrement=True)
+    username    = Column(String(50), unique=True, nullable=False)
+    password    = Column(String(50), nullable=False)
+    firstname   = Column(String(50), nullable=False)
+    lastname    = Column(String(50), nullable=False)
+    DoB         = Column(String(50), nullable=False)
+    phone       = Column(String(50), nullable=False)
+    email       = Column(String(100))
+    address     = Column(String(200), nullable=False)
+    roleID      = Column(Integer, nullable=False)
+    qnas        = relationship("Qna", backref="user", lazy= False)
 
 class Qna(database.Model):
     __name__ = "Question&Answer"
