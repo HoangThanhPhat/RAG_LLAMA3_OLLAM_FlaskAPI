8bb0fba1de7d54d6c806438abd5de5ec17479c35
thanhphathoang <125521127+HoangThanhPhat@users.noreply.github.com>
2024-05-15 17:14:03 +0700

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
-from flask import Flask, request, jsonify, render_template, redirect, url_for
-from werkzeug.security import check_password_hash
+from flask import Flask, request, jsonify
+from flask_bcrypt import check_password_hash
+from werkzeug.security import generate_password_hash
 from flask_cors import CORS
 from flask_bcrypt import generate_password_hash
 from langchain.chains.retrieval import create_retrieval_chain
@@ -11,13 +12,13 @@ from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain_community.document_loaders import PDFPlumberLoader
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.prompts import PromptTemplate
-from datetime import datetime
 from pymysql import IntegrityError
-from crt_db import database,User,Qna
+from crt_db import database,User,Qna, Conversation
+from datetime import datetime
 
 
 app = Flask(__name__)
-# CORS(app)
+CORS(app)
 
 # --------------------------------------------------------Connect database-----------------------------------------------------------------
 # My SQL
@@ -27,6 +28,7 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 # database = SQLAlchemy(app)
 database.init_app(app)
 # app.app_context().push()
+# database.create_all()
 # Khởi tạo cơ sở dữ liệu
 # with app.app_context():
 #     database = SQLAlchemy(app)
@@ -46,9 +48,8 @@ raw_prompt = PromptTemplate.from_template(
     """
     <s>[INST] Bạn là một trợ lý ảo thông minh. 
               Nếu như bạn không biết câu trả lời, hãy nói không biết, đừng cố tạo ra câu trả lời. 
-              Chỉ trả lời câu hỏi được đề ra.
-              Luôn trả lời bằng tiếng Việt Nam.
-              Trả lời chỗ nào cần xuống dòng thì xuống dòng, trình bày thật đẹp cho người dùng dễ hiểu.          
+              Chỉ trả lời câu hỏi ở phần query đưa ra, không trả lời thêm.
+              Luôn trả lời bằng tiếng Việt Nam.        
     [/INST] </s>
     [INST] {input}
             Context: {context}
@@ -57,6 +58,7 @@ raw_prompt = PromptTemplate.from_template(
     
     """
 )
+
 
 # Embedding
 embedding = FastEmbedEmbeddings()
@@ -93,11 +95,13 @@ def askPDFPost():
     query = json_content.get("query")
     print(f"query: {query}")
 # --------------------------------------Answer--------------------------------------------
+    # Lấy giá trị temprature từ yêu cầu JSON, mặc định là 0.7 nếu không có
+    temperature = json_content.get("temperature", 0.7)
     # Tải dữ liệu từ vectorstores
     print("Loading vector store")
     vector_store = Chroma(persist_directory=folder_path, embedding_function= embedding)
 
-    # Tạo train với tham số so sánh k=20, temparature = 0.1 (score_threshold)
+    # Tạo train với tham số so sánh k=20,
     print("Creating train")
     retriever = vector_store.as_retriever(
         search_type = "similarity_score_threshold",
@@ -111,6 +115,7 @@ def askPDFPost():
     document_chain = create_stuff_documents_chain(cached_llm, raw_prompt)
     chain = create_retrieval_chain(retriever, document_chain)
 
+
     # In result ra api
     result = chain.invoke({"input": query})
 
@@ -118,8 +123,16 @@ def askPDFPost():
     # Lấy câu trả lời từ result
     result_answer = result["answer"]
 
+    # Lấy thời gian hiện tại của hệ thống
+    current_time = datetime.utcnow()
+
     # Tạo một bản ghi mới cho bảng "qna"
-    new_qna = Qna(Question=str(query), Answer=str(result_answer), user_id = "1" )
+    new_qna = Qna(
+        Question=str(query),
+        Answer=str(result_answer),
+        conversation_id = "1",
+        created_at=current_time
+    )
     try:
         # Thêm bản ghi mới vào cơ sở dữ liệu
         database.session.add(new_qna)
@@ -133,6 +146,60 @@ def askPDFPost():
 
     response_answer = {"answer": result["answer"]}
     return response_answer
+
+# @app.route("/ask_pdf", methods=["POST"])
+# def askPDFPost():
+#
+#     try:
+#         print("Post /ask_pdf called")
+#         json_content = request.json
+#         query = json_content.get("query")
+#         print(f"query: {query}")
+#
+#         # Lấy user_id từ request
+#         user_id = json_content.get("user_id")
+#         print(user_id)
+#         # --------------------------------------Answer--------------------------------------------
+#         # Lấy giá trị temprature từ yêu cầu JSON, mặc định là 0.7 nếu không có
+#         temperature = json_content.get("temperature", 0.7)
+#         # Tải dữ liệu từ vectorstores
+#         print("Loading vector store")
+#         vector_store = Chroma(persist_directory=folder_path, embedding_function=embedding)
+#
+#         # Tạo train với tham số so sánh k=20,
+#         print("Creating train")
+#         retriever = vector_store.as_retriever(
+#             search_type="similarity_score_threshold",
+#             search_kwargs={
+#                 "k": 3,
+#                 "score_threshold": 0.03,
+#             },
+#         )
+#
+#         # Dùng model và đặt promp cho nó
+#         document_chain = create_stuff_documents_chain(cached_llm, raw_prompt)
+#         chain = create_retrieval_chain(retriever, document_chain)
+#
+#         # In result ra api
+#         result = chain.invoke({"input": query})
+#
+#         # --------------------------------------Add database------------------------------------------
+#         # Lấy câu trả lời từ result
+#         result_answer = result["answer"]
+#
+#         # Tạo một bản ghi mới trong bảng "Qna" với các thông tin nhận được
+#         new_qna = Qna(Question=str(query), Answer=str(result_answer), user_id="1")
+#         database.session.add(new_qna)
+#         database.session.commit()
+#
+#         response_answer = {"message": "Question added successfully"}
+#         return jsonify(response_answer), 200
+#     except Exception as e:
+#         database.session.rollback()
+#         print(f"Error adding question: {e}")
+#         response_answer = {"error": "Failed to add question"}
+#         return jsonify(response_answer), 500
+
 # --------------------------------------------------------------------------------------------------------------------------------------------------
 
 
@@ -172,6 +239,51 @@ def pdfPost():
     return response
 # ---------------------------------------------------------------------------------------------------------------------------------------
 
+# ----------------------------------------------------------------GET QnA----------------------------------------------------------
+@app.route("/qna/<user_id>", methods=["GET"])
+def get_qna_by_user_id(user_id):
+    try:
+        print(f"Requested user_id: {user_id}")  # In ra user_id được yêu cầu
+        # Lấy tất cả câu hỏi và câu trả lời từ cơ sở dữ liệu dựa trên user_id
+        qna_records = Qna.query.filter_by(user_id=user_id).all()
+
+        # Kiểm tra xem có bản ghi nào được trả về hay không
+        if qna_records:
+            # Tạo danh sách chứa thông tin của tất cả câu hỏi và câu trả lời
+            qna_data = []
+            for record in qna_records:
+                qna_info = {
+                    "question": record.Question,
+                    "answer": record.Answer,
+                    "user_id": record.user_id
+                }
+                qna_data.append(qna_info)
+
+            # Trả về danh sách câu hỏi và câu trả lời dưới dạng JSON
+            return jsonify(qna_data), 200
+        else:
+            # Nếu không tìm thấy bản ghi, trả về thông báo lỗi
+            return jsonify({"error": "No records found for the provided user_id"}), 404
+    except Exception as e:
+        # Xử lý các ngoại lệ và trả về thông báo lỗi
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/add_conversation", methods=["POST"])
+def add_conversation():
+    try:
+        user_id = request.json.get("user_id")
+        # Tạo một bản ghi mới của Conversation
+        new_conversation = Conversation(user_id=user_id)
+        database.session.add(new_conversation)
+        database.session.commit()
+        return jsonify({"message": "Conversation added successfully!"}), 201
+    except Exception as e:
+        database.session.rollback()
+        return jsonify({"error": str(e)}), 500
+
+# ---------------------------------------------------------------------------------------------------------------------------------------
+
 # ----------------------------------------------------------------CRUD USER--------------------------------------------------------------
 
 # -----------------Create User--------------------------
@@ -185,7 +297,7 @@ def create_user():
         firstname = data.get("firstname")
         lastname  = data.get("lastname")
         DoB       = data.get("DoB")
-        phone     = data.get("phone number")
+        phone     = data.get("phone")
         email     = data.get("email")
         address   = data.get("address")
         roleID    = data.get("roleID")
@@ -276,6 +388,37 @@ def get_all_users():
         return jsonify({"error": str(e)}), 500
 
 
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    try:
+        data = request.json
+        username = data.get('username')
+        password = data.get('password')
+
+        if not username or not password:
+            return jsonify({"error": "Username and password are required"}), 400
+
+        # Truy vấn người dùng từ cơ sở dữ liệu
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            # So sánh mật khẩu đã hash với mật khẩu nhập vào
+            if check_password_hash(user.password, password):
+                # Xác thực thành công, trả về user_id
+                return jsonify({"user_id": user.id}), 200
+            else:
+                # Xác thực thất bại
+                return jsonify({"error": "Invalid username or password"}), 401
+        else:
+            # Không tìm thấy người dùng
+            return jsonify({"error": "User not found"}), 404
+    except Exception as e:
+        # Xử lý các ngoại lệ và trả về thông báo lỗi
+        return jsonify({"error": str(e)}), 500
+
+
 # -----------------Update User--------------------------
 @app.route("/user/<username>", methods=["PUT"])
 def update_user(username):
@@ -287,8 +430,11 @@ def update_user(username):
         if user:
             # Cập nhật thông tin người dùng với dữ liệu được gửi trong yêu cầu
             data = request.json
-            user.username = data.get("username", user.username)
-            user.password = data.get("password", user.password)
+            # Kiểm tra nếu có mật khẩu mới được cung cấp
+            new_password = data.get("password")
+            if new_password:
+                # Hash mật khẩu mới trước khi lưu vào cơ sở dữ liệu
+                user.password = generate_password_hash(new_password).decode("utf-8")
             user.firstname = data.get("firstname", user.firstname)
             user.lastname = data.get("lastname", user.lastname)
             user.DoB = data.get("DoB", user.DoB)
@@ -335,6 +481,9 @@ def start_app():
     app.run(host= "0.0.0.0", port= 8080, debug=True)
 if __name__ == "__main__":
     start_app()
+
+
+
 
 
 
